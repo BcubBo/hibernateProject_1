d73dc95803563dbb78f431ac87143322050fa6b3
bowieu <fatalitywolf@foxmail.com>
2017-06-22 14:47:01 +0800

    编写可序列化类并在hibernate中配置
    Change-Id: Ibda96d1a4139e7100392b37260df7361503dd461
    Change-Id: 000001
    Signed-off-by: bowieu <fatalitywolf@foxmail.com>

---
+++ src/po/User.java
@@ -0,0 +1,93 @@
+package po;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+
+
+	private static final long serialVersionUID = -2998707288951184222L;
+	private Integer id;
+	private String name;
+	private String password;
+	private String telephone;
+	private String username;
+	private String isAdmin;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	//
+	
+	public String getName() {
+		return name;
+	}
+	
+	//
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	//
+	
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	//
+	
+	
+	public String getTelephone() {
+		return telephone;
+	}
+	
+	//
+	
+	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	//
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	//
+	
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	//
+	
+	
+	public String getIsAdmin() {
+		return isAdmin;
+	}
+	//
+	
+	
+	public void setIsAdmin(String isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	//
+	
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+	
+}
